a14fa27b2571f45bd46ec2c69ff4c42ade39dda9
haccht <haccht@users.noreply.github.com>
2024-06-30 19:08:24 +0900

    Update sdk

--- my_status.py
+++ my_status.py
@@ -1,42 +1,15 @@
-import os
-import json
-from dotenv import load_dotenv
-from requests_oauthlib import OAuth2Session
+from webexteamssdk import WebexTeamsAPI
 from unicornhat import UnicornHatHD
 
+api = WebexTeamsAPI()
 
-def token_save(token):
-    with open(ACCESS_TOKEN_FILE) as fp:
-        json.dump(token, fp, indent=2)
-
-
-load_dotenv()
-ACCESS_TOKEN_FILE = 'access_token.json'
-
-
-# load access token
-with open(ACCESS_TOKEN_FILE) as fp:
-    token_map = json.load(fp)
-
-oauth = OAuth2Session(
-        os.environ['CLIENT_ID'],
-        token=token_map,
-        auto_refresh_kwargs={
-            'client_id': os.environ['CLIENT_ID'],
-            'client_secret': os.environ['CLIENT_SECRET']},
-        auto_refresh_url='https://webexapis.com/v1/access_token',
-        token_updater=token_save)
-
-# query api
-resp = oauth.get('https://webexapis.com/v1/people/me')
-data = resp.json()
-
-print(f"{data['userName']} status={data['status']}")
+data = api.people.me()
+print(f"{data.userName} status={data.status}")
 
 
 # render emoji
 hat = UnicornHatHD()
-if data["status"] in ["call", "DoNotDisturb", "meeting", "presenting", "unknown"]:
+if data.status in ["call", "DoNotDisturb", "meeting", "presenting", "unknown"]:
     hat.draw_emoji('microphone')
 else:
     hat.clear()
